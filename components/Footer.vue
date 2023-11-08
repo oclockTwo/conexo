4ebc6d1456f46f410f9bfd345804bd267997fde1
BingNi <nibing1220@qq.com>
2023-11-08 15:40:01 +0800

    update footer and add terms

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,58 @@
 <template>
-  <footer class="mt-4">
+  <footer class="footer p-10 text-base-content">
+    <font></font>
+    <nav>
+      <font></font>
+      <header class="footer-title">Services</header>
+      <font></font> <a class="link link-hover" href="/">Conexo</a>
+      <font></font>
+      <a class="link link-hover" href="/conexoilimitado">Conexo Ilimitado</a>
+      <font></font>
+      <a class="link link-hover" href="/conexo-resposta-hoje"
+        >Conexo Resposta</a
+      >
+      <font></font>
+      <a class="link link-hover" href="/conexo-dias-anteriores"
+        >Conexo Dias Anteriores</a
+      ><font></font>
+    </nav>
+    <font></font>
+    <nav>
+      <font></font>
+      <header class="footer-title">Legal</header>
+      <font></font>
+      <a class="link link-hover" href="/terms/privacy-policy">Privacy Policy</a>
+      <font></font>
+      <a class="link link-hover" href="/terms/terms-of-service"
+        >Terms of Service</a
+      >
+      <font></font>
+    </nav>
+    <font></font>
+    <nav>
+      <font></font>
+      <header class="footer-title">Social</header>
+      <font></font>
+      <div class="grid grid-flow-col gap-4">
+        <font></font>
+        <a href="https://www.facebook.com/groups/3590757467842210/"
+          ><svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            class="fill-current"
+          >
+            <path
+              d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"
+            ></path></svg></a
+        ><font></font>
+      </div>
+      <font></font>
+    </nav>
+    <font></font>
+  </footer>
+  <!-- <footer class="mt-4">
     <div class="mx-auto px-4 leading-7 text-black">
       <div class="flex items-center justify-center text-black">
         <div class="text-xs font-normal leading-3 text-slate-800">
@@ -24,7 +77,7 @@
         conexojogo.com is not affiliated with any other games in any way
       </div>
     </div>
-  </footer>
+  </footer> -->
 </template>
 
 <script setup></script>
