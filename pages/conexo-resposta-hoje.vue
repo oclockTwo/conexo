64f902eaac9c705d4063989710dd2dd256bee174
BingNi <nibing1220@qq.com>
2023-11-20 16:51:20 +0800

    remove ad in resposta page

--- pages/conexo-resposta-hoje.vue
+++ pages/conexo-resposta-hoje.vue
@@ -238,21 +238,6 @@
       </div>
     </div>
   </div>
-  <script
-    async
-    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8993913594775765"
-    crossorigin="anonymous"
-  ></script>
-  <ins
-    class="adsbygoogle"
-    style="display: block"
-    data-ad-format="autorelaxed"
-    data-ad-client="ca-pub-8993913594775765"
-    data-ad-slot="6021779601"
-  ></ins>
-  <script>
-    (adsbygoogle = window.adsbygoogle || []).push({});
-  </script>
   <Footer />
 </template>
 
