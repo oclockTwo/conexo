fed122039f3622d2aeca9508faac7585633ac3c7
BingNi <nibing1220@qq.com>
2023-12-04 13:28:44 +0800

    daily update

--- pages/conexoilimitado.vue
+++ pages/conexoilimitado.vue
@@ -58,7 +58,7 @@
           @change="selectGameData"
         >
           <option disabled selected>1/10/2023</option>
-          <option v-for="(value, key) in filteredData(0, 62)" :key="key">
+          <option v-for="(value, key) in filteredData(0, 63)" :key="key">
             {{ keyToDate(Number(key)) }}
           </option>
         </select>
