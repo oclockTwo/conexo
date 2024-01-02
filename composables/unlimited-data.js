edcdc401929890f82ab3a2a36563ed74a9779a68
BingNi <nibing1220@qq.com>
2024-01-02 23:01:22 +0800

    add data to 3

--- composables/unlimited-data.js
+++ composables/unlimited-data.js
@@ -3947,6 +3947,90 @@ const data = {
       "capuz",
     ],
   },
+  94: {
+    groups: [
+      {
+        number: 1,
+        theme: "Órgãos dos sentidos",
+        words: ["olhos", "ouvidos", "nariz", "pele"],
+      },
+      {
+        number: 2,
+        theme: "Coisas que países tem",
+        words: ["bandeira", "língua", "moeda", "fronteiras"],
+      },
+      {
+        number: 3,
+        theme: "Sinônimos de bens ou riqueza",
+        words: ["capital", "fortuna", "espólio", "fundos"],
+      },
+      {
+        number: 4,
+        theme: "Relacionados a piratas",
+        words: ["tesouro", "navio", "papagaio", "mapa"],
+      },
+    ],
+    startingBoard: [
+      "navio",
+      "espólio",
+      "fortuna",
+      "ouvidos",
+      "pele",
+      "olhos",
+      "mapa",
+      "nariz",
+      "tesouro",
+      "moeda",
+      "capital",
+      "fronteiras",
+      "bandeira",
+      "papagaio",
+      "fundos",
+      "língua",
+    ],
+  },
+  95: {
+    groups: [
+      {
+        number: 1,
+        theme: "Dedos",
+        words: ["indicador", "médio", "anelar", "mínimo"],
+      },
+      {
+        number: 2,
+        theme: "Que é muito pequeno",
+        words: ["ínfimo", "diminuto", "minúsculo", "microscópico"],
+      },
+      {
+        number: 3,
+        theme: "Animais que vivem no gelo",
+        words: ["morsa", "foca", "pinguim", "beluga"],
+      },
+      {
+        number: 4,
+        theme: "O Pequeno ___",
+        words: ["príncipe", "polegar", "stuart little", "urso"],
+      },
+    ],
+    startingBoard: [
+      "microscópico",
+      "anelar",
+      "stuart little",
+      "indicador",
+      "pinguim",
+      "médio",
+      "polegar",
+      "diminuto",
+      "ínfimo",
+      "foca",
+      "mínimo",
+      "morsa",
+      "beluga",
+      "minúsculo",
+      "urso",
+      "príncipe",
+    ],
+  },
 };
 
 export const useUnlimitedData = () => {
